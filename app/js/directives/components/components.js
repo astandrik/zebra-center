6b36d031c6005457b5018b141ceafd8cbca466ff
Anton <astandrik@gmail.com>
2016-12-25 20:33:22 +0300

    styles and footer

--- app/js/directives/components/components.js
+++ app/js/directives/components/components.js
@@ -1,7 +1,9 @@
 var header = require('./header.js');
 var navbar = require('./navbar.js');
 var sidebar = require('./sidebar.js');
-var currentModule = angular.module('components',[]);
+var footer = require('./footer.js');
+var currentModule = angular.module('components', []);
 currentModule.component(header.name, header.component);
 currentModule.component(navbar.name, navbar.component);
 currentModule.component(sidebar.name, sidebar.component);
+currentModule.component(footer.name, footer.component);
---
+++ app/js/directives/components/footer.js
@@ -0,0 +1,9 @@
+var footer = {
+    templateUrl: 'views/components/footer.html',
+}
+
+module.exports = {
+    name: 'footerDirective',
+    type: 'component',
+    component: footer
+}
