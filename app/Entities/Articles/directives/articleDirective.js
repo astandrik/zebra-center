c549a081a7b4153424aff88c346b3ae73d283ebe
Anton <astandrik@gmail.com>
2016-12-26 23:44:37 +0300

    broken names

--- app/Entities/Articles/directives/articleDirective.js
+++ app/Entities/Articles/directives/articleDirective.js
@@ -4,7 +4,7 @@ var transliterate = (
     function () {
         var
             rus = "щ   ш  ч  ц  ю  я  ё  ж  ъ  ы  э  а б в г д е з и й к л м н о п р с т у ф х ь".split(/ +/g),
-            eng = "shh sh ch cz yu ya yo zh `` y' e` a b v g d e z i j k l m n o p r s t u f x `".split(/ +/g);
+            eng = "shh sh ch cz yu ya yo zh jj yj ej a b v g d e z i j k l m n o p r s t u f x j".split(/ +/g);
         return function (text, engToRus) {
             var x;
             for (x = 0; x < rus.length; x++) {
