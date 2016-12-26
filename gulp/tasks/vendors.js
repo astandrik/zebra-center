0feb6602e4c1743ae6e985a728740151631ea5cd
Anton <astandrik@gmail.com>
2016-12-26 22:35:14 +0300

    removed some files

--- gulp/tasks/vendors.js
+++ gulp/tasks/vendors.js
@@ -2,7 +2,6 @@ import gulp from 'gulp';
 
 gulp.task('vendors', function () {
     return gulp.src(['node_modules/angular-gridster/dist/*',
-                   'node_modules/angular-ui-tree/dist/*',
-                   'node_modules/javascript-detect-element-resize/jquery.resize.js'])
+                   'node_modules/angular-ui-tree/dist/*'])
         .pipe(gulp.dest('build/vendors'));
 })
