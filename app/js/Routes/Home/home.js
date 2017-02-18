e883323b6d5b1051074206284f62285a67ecc04e
Anton <astandrik@gmail.com>
2017-02-19 01:47:41 +0300

    recalculate articles grid on resize with debounce

--- app/js/Routes/Home/home.js
+++ app/js/Routes/Home/home.js
@@ -23,6 +23,20 @@ function fitHeight(timeout, article) {
     });
 }
 
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
 
 var entity = () => ({
     url: '/',
@@ -45,8 +59,10 @@ var entity = () => ({
                                 id: article.id
                             });
                         }
+                        $scope.$digest();
                     }, 100);
                 }
+                $( window ).resize(debounce(stopFunction,500));
                 var isUserAdmin = $rootScope.enableEditing;
                 $rootScope.$watch('enableEditing', function (newVal, oldVal) {
                     gridOptions.resizable.enabled = newVal;
--- app/js/Routes/Section/articlesList.js
+++ app/js/Routes/Section/articlesList.js
@@ -25,6 +25,21 @@ function fitHeight(timeout, article) {
 }
 
 
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 
 var entity = () => ({
     url: '/section/:viewAlias',
@@ -50,6 +65,7 @@ var entity = () => ({
                         }
                     }, 100);
                 }
+                $( window ).resize(debounce(stopFunction,500));
                 var isUserAdmin = $rootScope.enableEditing;
                 $rootScope.$watch('enableEditing', function (newVal, oldVal) {
                     gridOptions.resizable.enabled = newVal;
